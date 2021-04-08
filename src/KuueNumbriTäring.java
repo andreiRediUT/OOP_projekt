0df7029fda160b8fd3b9f67c84d73284687dcc4c
Andrei Redi <andrei.redi@ut.ee>
2021-04-08 16:48:29 +0300

    Veel täiendusi.

--- src/KuueNumbriTäring.java
+++ src/KuueNumbriTäring.java
@@ -1,24 +1,17 @@
-    //Täringumäng.
-// Koostada klassid Täring ja Täringumäng jäljendamaks kahe mängija mängu.
-// Mängijate viskevoorud kordamööda.
-//Ühes viskevoorus võib mängija visata täringuid ükskõik mitu korda, aga kui tuleb 1, siis kogusumma nullitakse ja viskeõigus läheb teisele mängijale.
-// Muidu viskel saadud tulemus liidetakse kogusummale. Võidab see, kes saab enne üle 91 punkti.
-
 
 public class KuueNumbriTäring {
-    int tulemus;
+
     int veeretus;
 
     /**
-     * Konstruktor määrab algtulemuseks 0 ning genereerib täringuveeretusele esimese juhusliku nubri
+     * Konstruktor genereerib täringuveeretusele esimese juhusliku numbri
      */
     public KuueNumbriTäring() {
-        this.tulemus = 0;
         this.veeretus = (int) (Math.random() * 6 + 1);
     }
 
     /**
-     *  Meetod genereerib numbri 1-6ni.
+     * Meetod genereerib numbri 1-6ni.
      */
     public void täringuVeeretus() {
 
@@ -26,20 +19,11 @@ public class KuueNumbriTäring {
     }
 
     /**
-     *
-     * @param veeretus võtab veeretuse ja lisab tulemusele
-     */
-    public void Tulemus(int veeretus){
-
-        tulemus +=veeretus;
-    }
-
-    /**
-     *
      * @return nende get meetoditega tagastakse vastavad väärtused
      */
 
-    public int getTulemus() { return tulemus; }
-    public int getVeeretus() {return veeretus;}
-
+    public int getVeeretus() {
+        return veeretus;
     }
+
+}
