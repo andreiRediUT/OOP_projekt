2f2e44d9eb8269cd71e32157cef2cd6bd1e36ba9
Priit Rooden <73076712+priitro@users.noreply.github.com>
2021-04-08 14:29:50 +0300

    Update KuueNumbriTäring.java
    Korrastasin faili sisu, võtsin üleliigse välja Tulemus - seda ei kasutatud

--- src/KuueNumbriTäring.java
+++ src/KuueNumbriTäring.java
@@ -1,10 +1,6 @@
 //Täringumäng.
-// Koostada klassid Täring ja Täringumäng jäljendamaks kahe mängija mängu.
-// Mängijate viskevoorud kordamööda.
-//Ühes viskevoorus võib mängija visata täringuid ükskõik mitu korda, aga kui tuleb 1, siis kogusumma nullitakse ja viskeõigus läheb teisele mängijale.
-// Muidu viskel saadud tulemus liidetakse kogusummale. Võidab see, kes saab enne üle 91 punkti.
-
-
+// Klass täringu veeretustulemuse saamiseks
+//täringuVeeretus tuleb meetodiga Math.random ja saadakse kätte getVeeretus() ega
 public class KuueNumbriTäring {
     int tulemus;
     int veeretus;
@@ -18,12 +14,7 @@ public class KuueNumbriTäring {
 
         this.veeretus = (int) (Math.random() * 6 + 1);
     }
-    public void Tulemus(int veeretus){
 
-        tulemus +=veeretus;
-    }
-
-    public int getTulemus() { return tulemus; }
     public int getVeeretus() {return veeretus;}
 
-    }
+}
