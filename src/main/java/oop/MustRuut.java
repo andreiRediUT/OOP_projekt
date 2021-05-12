70833aba08d4ce9e416fe153e8d83baf200d1799
Andrei Redi <andrei.redi@ut.ee>
2021-05-12 13:51:22 +0300

    Läksin vanale commitile, projekt läks katki

--- src/main/java/oop/MustRuut.java
+++ src/main/java/oop/MustRuut.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -12,14 +13,18 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class MustRuut extends Application {
+
 
     KuueNumbriTäring täring = new KuueNumbriTäring();
 
@@ -33,11 +38,7 @@ public class MustRuut extends Application {
     @Override
     public void start(Stage primaryStage) throws FileNotFoundException {
         Button btn1 = new Button("Veereta");
-        btn1.setLayoutX(300);
-        btn1.setLayoutY(250);
         Button btn2 = new Button("Anna käik üle.");
-        btn2.setLayoutX(500);
-        btn2.setLayoutY(250);
 
 
 //        Image piltFail = new Image(new FileInputStream("C:\\Users\\redjo\\proge\\Oop\\OOP_projekt\\src\\main\\java\\oop\\pildid\\täring.png"));
@@ -49,14 +50,19 @@ public class MustRuut extends Application {
         Text veeretus = new Text("0");
         Text skoor = new Text("0");
         skoor.setFont(Font.font(50));
+
         VBox veeretus_skoor = new VBox(veeretus, skoor );
+        veeretus_skoor.setAlignment(Pos.CENTER);
+
+
         veeretus.setFont(Font.font("Verdana", 100));
 
         BorderPane border = new BorderPane();
-        border.setPadding(new Insets(10, 20, 10, 20));
-
 
         HBox nupud = new HBox( btn1, btn2);
+        nupud.setAlignment(Pos.CENTER);
+
+
         nupud.setSpacing(10);
         VBox kesk = new VBox(veeretus_skoor, nupud);
 
@@ -64,36 +70,13 @@ public class MustRuut extends Application {
         kesk.setPadding(new Insets(15, 100, 15, 100));
         nupud.setCenterShape(true);
 
-        Text nimetus1 = new Text("Mängija 1");
-        Text nimetus2 = new Text("Mängija 2");
-        nimetus1.setFont(Font.font("Verdana", 20));
-        nimetus2.setFont(Font.font("Verdana", 20));
-        Text mängija1 = new Text("0");
-        Text mängija2 = new Text("0");
-        mängija1.setFont(Font.font("Verdana", 100));
-        mängija2.setFont(Font.font("Verdana", 100));
-
-        VBox vasak = new VBox(
-                nimetus1,
-                mängija1
-
-        );
-        VBox parem = new VBox(nimetus2, mängija2);
-
-
-        Text t = new Text(10, 20, "TÄRINGUMÄNG");
-        t.setFont(Font.font("Verdana", 25));
-        StackPane pealkiri = new StackPane(t);
-
-
         border.setCenter(kesk);
-        border.setTop(pealkiri);
-        border.setLeft(vasak);
-        border.setRight(parem);
+        border.setTop(title());
+        border.setLeft(mängija("Mängija 1", 0, true));
+        border.setRight(mängija("Mängija 2", 0 , false));
 
 
-        Scene scene = new Scene(border, 600, 300);
-
+        Scene scene = new Scene(border, 700 , 300);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Täringumäng");
         primaryStage.show();
@@ -102,6 +85,9 @@ public class MustRuut extends Application {
         btn1.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                if (mängu_skoor.keegiVõitnud() == -1) {
+                    // TODO võiduvärk
+                }
                 täring.täringuVeeretus();
 
                 veeretus.setText(Integer.toString(täring.getVeeretus()));
@@ -112,6 +98,9 @@ public class MustRuut extends Application {
                 if (täring.getVeeretus() == 1)
                     mängu_skoor.poole_vahetus(true);
 
+                border.setLeft(mängija("Mängija 1 ", mängu_skoor.getSkoor()[0], mängu_skoor.isEsimese_kord()));
+                border.setRight(mängija("Mängija 2 ", mängu_skoor.getSkoor()[1], !mängu_skoor.isEsimese_kord()));
+
             }
         });
 
@@ -120,16 +109,52 @@ public class MustRuut extends Application {
             @Override
             public void handle(ActionEvent event) {
 
+
                 mängu_skoor.liidaSkoor();
                 mängu_skoor.poole_vahetus(false);
+                border.setLeft(mängija("Mängija 1 ", mängu_skoor.getSkoor()[0], mängu_skoor.isEsimese_kord()));
+                border.setRight(mängija("Mängija 2 ", mängu_skoor.getSkoor()[1], !mängu_skoor.isEsimese_kord()));
+                System.out.println(Arrays.toString(mängu_skoor.getSkoor()));
 
-                mängija1.setText(Integer.toString(mängu_skoor.getSkoor()[0]));
-                mängija2.setText(Integer.toString(mängu_skoor.getSkoor()[1]));
+
                 skoor.setText("0");
                 veeretus.setText("");
 
             }
         });
     }
+
+    private VBox mängija (String nimi, int skoor, boolean tema_kord) {
+        Text nimetus = new Text(nimi);
+        nimetus.setFont(Font.font("Verdana", 20));
+        Text m_skoor = new Text(Integer.toString(skoor));
+
+        m_skoor.setFont(Font.font("Verdana", 100));
+
+        if (tema_kord)
+            return new VBox(  nimetus,  m_skoor, kelle_kord() );
+
+        return new VBox(nimetus, m_skoor);
+    }
+
+    private Text kelle_kord() {
+        Text nimetus = new Text("Kord");
+        nimetus.setFont(Font.font("Verdana", FontWeight.BOLD,  20));
+        nimetus.resize(200, 200);
+        nimetus.setFill(Color.RED);
+        nimetus.setStyle("-colour: #FF0000;");
+        return nimetus;
+    }
+
+    private StackPane title () {
+        Text t = new Text(10, 20, "TÄRINGUMÄNG");
+        t.setFont(Font.font("Verdana", 25));
+        StackPane pealkiri = new StackPane(t);
+
+        pealkiri.setStyle("-fx-background-colour: #FF0000;");
+
+        return pealkiri;
+    }
+
 
 }
